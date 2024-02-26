b62670bcc65b5af9bbe360896da145834542625c
Joko <kalle0045@gmail.com>
2024-02-26 10:12:27 +0100

    Login

--- src/App.tsx
+++ src/App.tsx
@@ -1,79 +1,39 @@
-import {useState} from "react";
-import axios from "axios";
+import {useEffect} from "react";
+import {BrowserRouter, Route, Routes, useLocation} from "react-router-dom";
+import Login from "./pages/Login.tsx";
+import Dashboard from "./pages/Dashboard.tsx";
 
 function App() {
-    // const apiData = await ApiService.fetchGet('/setting', urlParamsObject);
-    const [username, setUsername] = useState('admin');
-    const [password, setPassword] = useState('admin');
-
-    const handleSubmit = async (e: any) => {
-        e.preventDefault();
-        try {
-            const response = await axios.post('/api/auth', {
-                username,
-                password,
-            }, {
-                headers: {
-                    'Content-Type': 'application/json',
-                    'Access-Control-Allow-Headers': 'Origin, X-Requested-With, Content-Type, Accept, authorization'
-                },
-                withCredentials: true
-            });
-            console.log(response);
-        } catch (error) {
-            console.error(error);
-        }
-    };
-
-    const checkSession = async () => {
-        try {
-            const response = await axios.get('/api/checkSession', {
-                headers: {
-                    'Content-Type': 'application/json',
-                    'Access-Control-Allow-Headers': 'Origin, X-Requested-With, Content-Type, Accept, authorization'
-                },
-                withCredentials: true});
-            console.log(response.data);
-        } catch (error) {
-            console.error(error);
-        }
-    }
-
     return (
-        <>
-            <button onClick={checkSession}>checkSession</button>
-            <form onSubmit={handleSubmit}>
-                <input
-                    type="text"
-                    placeholder="username"
-                    value={username}
-                    onChange={(e) => setUsername(e.target.value)}
-                    required
-                />
-                <input
-                    type="password"
-                    placeholder="Password"
-                    value={password}
-                    onChange={(e) => setPassword(e.target.value)}
-                    required
-                />
-                <button type="submit">Login</button>
-            </form>
-            <div className='container'>
-                <div className='row'>
-                    <div className='col-12 md:col-6'>1</div>
-                    <div className='col-12 md:col-6'>1</div>
-                </div>
-            </div>
-            <div className='bg-indigo-300 mt-5'>Test</div>
-            <div className='container'>
-                <div className='row'>
-                    <div className='col-12 md:col-6'>1</div>
-                    <div className='col-12 md:col-6'>1</div>
-                </div>
-            </div>
-        </>
-    )
+        <BrowserRouter>
+            <Routes>
+                <Route index path="/" element={<LoginWithLocation />} />
+                <Route path="/dashboard" element={<DashboardWithLocation />} />
+            </Routes>
+        </BrowserRouter>
+    );
 }
 
-export default App
+export default App;
+
+function LoginWithLocation() {
+    const location = useLocation();
+
+    useEffect(() => {
+        console.log('URL hat sich geändert:', location.pathname);
+        // Weitere Logik für URL-Änderungen hier
+    }, [location.pathname]);
+
+    return <Login />;
+}
+
+function DashboardWithLocation() {
+    const location = useLocation();
+
+    useEffect(() => {
+        console.log('URL hat sich geändert:', location.pathname);
+        // Weitere Logik für URL-Änderungen hier
+    }, [location.pathname]);
+
+    return <Dashboard />;
+}
---
+++ src/pages/Login.tsx
@@ -0,0 +1,85 @@
+import {useState} from "react";
+import axios from "axios";
+import {Link} from "react-router-dom";
+
+const Login = () => {
+
+    // const apiData = await ApiService.fetchGet('/setting', urlParamsObject);
+    const [username, setUsername] = useState('admin');
+    const [password, setPassword] = useState('admin');
+
+    const handleSubmit = async (e: any) => {
+        e.preventDefault();
+        try {
+            const response = await axios.post('/api/auth', {
+                username,
+                password,
+            }, {
+                headers: {
+                    'Content-Type': 'application/json',
+                    'Access-Control-Allow-Headers': 'Origin, X-Requested-With, Content-Type, Accept, authorization'
+                },
+                withCredentials: true
+            });
+            console.log(response);
+        } catch (error) {
+            console.error(error);
+        }
+    };
+
+    const checkSession = async () => {
+        try {
+            const response = await axios.get('/api/checkSession', {
+                headers: {
+                    'Content-Type': 'application/json',
+                    'Access-Control-Allow-Headers': 'Origin, X-Requested-With, Content-Type, Accept, authorization'
+                },
+                withCredentials: true
+            });
+            console.log(response.data);
+        } catch (error) {
+            console.error(error);
+        }
+    }
+
+    return (
+        <>
+            <div>
+                <Link to={'/dashboard'}>APP</Link>
+            </div>
+            <button onClick={checkSession}>checkSession</button>
+            <form onSubmit={handleSubmit}>
+                <input
+                    type="text"
+                    placeholder="username"
+                    value={username}
+                    onChange={(e) => setUsername(e.target.value)}
+                    required
+                />
+                <input
+                    type="password"
+                    placeholder="Password"
+                    value={password}
+                    onChange={(e) => setPassword(e.target.value)}
+                    required
+                />
+                <button type="submit">Login</button>
+            </form>
+            <div className='container'>
+                <div className='row'>
+                    <div className='col-12 md:col-6'>1</div>
+                    <div className='col-12 md:col-6'>1</div>
+                </div>
+            </div>
+            <div className='bg-indigo-300 mt-5'>Test</div>
+            <div className='container'>
+                <div className='row'>
+                    <div className='col-12 md:col-6'>1</div>
+                    <div className='col-12 md:col-6'>1</div>
+                </div>
+            </div>
+        </>
+    )
+}
+
+export default Login;
