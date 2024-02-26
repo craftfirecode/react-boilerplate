b4c7f007e0885898206c17c90b3cf00121ecd1b7
Joko <kalle0045@gmail.com>
2024-02-26 13:28:42 +0100

    Add React Teample

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -38,34 +38,39 @@ const Login = () => {
             <div>
                 <Link to={'/dashboard'}>APP</Link>
             </div>
-            <form onSubmit={handleSubmit}>
-                <input
-                    type="text"
-                    placeholder="username"
-                    value={username}
-                    onChange={(e) => setUsername(e.target.value)}
-                    required
-                />
-                <input
-                    type="password"
-                    placeholder="Password"
-                    value={password}
-                    onChange={(e) => setPassword(e.target.value)}
-                    required
-                />
-                <button type="submit">Login</button>
-            </form>
             <div className='container'>
                 <div className='row'>
-                    <div className='col-12 md:col-6'>1</div>
-                    <div className='col-12 md:col-6'>1</div>
-                </div>
-            </div>
-            <div className='bg-indigo-300 mt-5'>Test</div>
-            <div className='container'>
-                <div className='row'>
-                    <div className='col-12 md:col-6'>1</div>
-                    <div className='col-12 md:col-6'>1</div>
+                    <div className='col-12'>
+                        <h1 className='mb-3'>Anmelden</h1>
+                        <form onSubmit={handleSubmit} className="flex flex-col space-y-4">
+                            <div className='row'>
+                                <div className='col-12 mt-3 md:col-6'>
+                                    <input
+                                        type="text"
+                                        placeholder="username"
+                                        value={username}
+                                        onChange={(e) => setUsername(e.target.value)}
+                                        required
+                                        className="appearance-none rounded-md px-3 py-2 border border-gray-300 focus:outline-none focus:ring-2 focus:ring-blue-400 w-full"
+                                    />
+                                </div>
+                                <div className='col-12 mt-3 md:col-6'>
+                                    <input
+                                        type="password"
+                                        placeholder="Password"
+                                        value={password}
+                                        onChange={(e) => setPassword(e.target.value)}
+                                        required
+                                        className="appearance-none rounded-md px-3 py-2 border border-gray-300 focus:outline-none focus:ring-2 focus:ring-blue-400 w-full"
+                                    />
+                                </div>
+                            </div>
+                            <button type="submit"
+                                    className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-white">
+                                Login
+                            </button>
+                        </form>
+                    </div>
                 </div>
             </div>
         </>
