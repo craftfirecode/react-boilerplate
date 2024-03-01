caeca56165fdf64f38b047d9675a22dcd508c479
Joko <kalle0045@gmail.com>
2024-03-01 10:13:32 +0100

    Add superbase js

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,17 @@
 // App.js
 import {BrowserRouter, Route, Routes} from "react-router-dom";
-import Login from "./pages/Login.tsx";
 import Home from "./pages/Home.tsx";
 import Konto from "./pages/Konto.tsx";
 import {ContentProvider} from "./context/ContentContext.tsx";
-import {useSession} from "./context/useSession.tsx";
 import TemplateMain from "./Template/TemplateMain.tsx";
 import NotFound from "./pages/NotFound.tsx";
+import {Auth} from "@supabase/auth-ui-react";
+import {ThemeSupa} from "@supabase/auth-ui-shared";
+import {createClient} from '@supabase/supabase-js'
+import Login from "./pages/Login.tsx";
+import {useSession} from "./context/useSession.tsx";
+
+const supabase = createClient('https://jfgrqcvupvyzyquawwpg.supabase.co', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImpmZ3JxY3Z1cHZ5enlxdWF3d3BnIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDkyODIwMjEsImV4cCI6MjAyNDg1ODAyMX0.D-O2nSRD3N4WWQOLc-aU3lOWof5tqTx3XriGTEpihDQ')
 
 function App() {
     const loggedIn = useSession();
@@ -16,18 +21,22 @@ function App() {
                 <BrowserRouter>
                     <Routes>
                         <Route path="/" element={<TemplateMain/>}>
-                            {loggedIn.loggedIn ? (
-                                <>
-                                    <Route path="/" element={<Home/>}/>
-                                    <Route path="/konto" element={<Konto/>}/>
-                                    <Route path="*" element={<NotFound/>}/>
-                                </>
-                            ) : (
-                                <>
-                                    <Route path="/" element={<Login/>}/>
-                                    <Route path="*" element={<NotFound/>}/>
-                                </>
-                            )
+                            {
+                                !loggedIn ? (
+                                        <>
+                                            <Route path="/" element={<Login><Auth supabaseClient={supabase}
+                                                                                  appearance={{theme: ThemeSupa}}/></Login>}/>
+                                            <Route path="*" element={<NotFound/>}/>
+                                        </>
+                                    )
+                                    :
+                                    (
+                                        <>
+                                            <Route path="/" element={<Home/>}/>
+                                            <Route path="/konto" element={<Konto/>}/>
+                                            <Route path="*" element={<NotFound/>}/>
+                                        </>
+                                    )
                             }
                         </Route>
                     </Routes>
