e36328d4e5f36dab6db33bceb301e99af8e8029d
Joko <kalle0045@gmail.com>
2024-03-29 14:21:23 +0100

    wow button cta

--- src/Template/TemplateMain.tsx
+++ src/Template/TemplateMain.tsx
@@ -1,11 +1,28 @@
 import {Outlet} from "react-router-dom";
 import Footer from "../component/Footer.tsx";
 import Nav from "../component/Nav.tsx";
+import HeroMenu from "../component/HeroMenu.tsx";
+import PopOver from "../component/PopOver.tsx";
 
 const Layout = () => {
     return (
         <div className="flex flex-col min-h-screen">
             <Nav />
+            <HeroMenu />
+            Demo
+            <div className="ms-auto">
+                <ul>
+                    <li>
+                       1 <PopOver />
+                    </li>
+                    <li>
+                       1 <PopOver />
+                    </li>
+                </ul>
+                <PopOver />
+                <PopOver />
+
+            </div>
             <main className="flex-1 flex my-5">
                 <Outlet />
             </main>
