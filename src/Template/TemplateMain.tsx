84498168a59c317225ed0692bff128c6550d16e6
Joko <kalle0045@gmail.com>
2024-03-29 15:14:55 +0100

    wow button cta

--- src/Template/TemplateMain.tsx
+++ src/Template/TemplateMain.tsx
@@ -1,12 +1,14 @@
 import {Outlet} from "react-router-dom";
 import Footer from "../component/Footer.tsx";
 import Nav from "../component/Nav.tsx";
+import HeroMenu from "../component/HeroMenu.tsx";
 
 const Layout = () => {
     return (
         <div className="flex flex-col min-h-screen">
 
             <Nav/>
+            <HeroMenu/>
             <main className="flex-1 flex my-5">
                 <Outlet/>
             </main>
