1e65140fc6383ee5e0ac9cf041cba682cf90dcc8
Joko <kalle0045@gmail.com>
2024-03-30 11:39:59 +0100

    wow button cta

--- src/component/NavigationMenuDemo.tsx
+++ src/component/NavigationMenuDemo.tsx
@@ -8,10 +8,10 @@ const NavigationMenuDemo = () => {
         <NavigationMenu.Root className="relative z-[1] flex w-screen justify-center">
             <NavigationMenu.List className="center shadow-blackA4 m-0 flex list-none rounded-[6px] bg-white p-1 shadow-[0_2px_10px]">
                 <NavigationMenu.Item>
-                    <NavigationMenu.Trigger className="text-violet11 hover:bg-violet3 focus:shadow-violet7 group flex select-none items-center justify-between gap-[2px] rounded-[4px] px-3 py-2 text-[15px] font-medium leading-none outline-none focus:shadow-[0_0_0_2px]">
+                    <NavigationMenu.Trigger className="text-violet11 hover:bg-violet-400 focus:shadow-violet-700 group flex select-none items-center justify-between gap-[2px] rounded-[4px] px-3 py-2 text-[15px] font-medium leading-none outline-none focus:shadow-[0_0_0_2px]">
                         Learn{' '}
                         <CaretDownIcon
-                            className="text-violet10 relative top-[1px] transition-transform duration-[250] ease-in group-data-[state=open]:-rotate-180"
+                            className="text-violet-100 relative top-[1px] transition-transform duration-[250] ease-in group-data-[state=open]:-rotate-180"
                             aria-hidden
                         />
                     </NavigationMenu.Trigger>
@@ -20,7 +20,7 @@ const NavigationMenuDemo = () => {
                             <li className="row-span-3 grid">
                                 <NavigationMenu.Link asChild>
                                     <a
-                                        className="focus:shadow-violet7 from-purple9 to-indigo9 flex
+                                        className="focus:shadow-violet-700 from-purple-900 to-indigo-900 flex
                     h-full w-full select-none flex-col justify-end rounded-[6px] bg-gradient-to-b p-[25px] no-underline outline-none focus:shadow-[0_0_0_2px]"
                                         href="/"
                                     >
@@ -53,10 +53,10 @@ const NavigationMenuDemo = () => {
                 </NavigationMenu.Item>
 
                 <NavigationMenu.Item>
-                    <NavigationMenu.Trigger className="text-violet11 hover:bg-violet3 focus:shadow-violet7 group flex select-none items-center justify-between gap-[2px] rounded-[4px] px-3 py-2 text-[15px] font-medium leading-none outline-none focus:shadow-[0_0_0_2px]">
+                    <NavigationMenu.Trigger className="text-violet11 hover:bg-violet-300 focus:shadow-violet7 group flex select-none items-center justify-between gap-[2px] rounded-[4px] px-3 py-2 text-[15px] font-medium leading-none outline-none focus:shadow-[0_0_0_2px]">
                         Overview{' '}
                         <CaretDownIcon
-                            className="text-violet10 relative top-[1px] transition-transform duration-[250] ease-in group-data-[state=open]:-rotate-180"
+                            className="text-violet-100 relative top-[1px] transition-transform duration-[250] ease-in group-data-[state=open]:-rotate-180"
                             aria-hidden
                         />
                     </NavigationMenu.Trigger>
@@ -86,7 +86,7 @@ const NavigationMenuDemo = () => {
 
                 <NavigationMenu.Item>
                     <NavigationMenu.Link
-                        className="text-violet11 hover:bg-violet3 focus:shadow-violet7 block select-none rounded-[4px] px-3 py-2 text-[15px] font-medium leading-none no-underline outline-none focus:shadow-[0_0_0_2px]"
+                        className="text-violet11 hover:bg-violet-300 focus:shadow-violet7 block select-none rounded-[4px] px-3 py-2 text-[15px] font-medium leading-none no-underline outline-none focus:shadow-[0_0_0_2px]"
                         href="https://github.com/radix-ui"
                     >
                         Github
@@ -105,7 +105,7 @@ const NavigationMenuDemo = () => {
     );
 };
 
-const ListItem = React.forwardRef(({ className, children, title, ...props }, forwardedRef) => (
+const ListItem = React.forwardRef(({ className, children, title, ...props }: any, forwardedRef) => (
     <li>
         <NavigationMenu.Link asChild>
             <a
