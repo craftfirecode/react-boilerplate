20f26715851a9fbc68859206479e9ec3279760a2
Joko <kalle0045@gmail.com>
2024-04-02 13:01:16 +0200

    arrow

--- src/component/Nav.tsx
+++ src/component/Nav.tsx
@@ -68,7 +68,12 @@ const Nav = () => {
                                             <Link to={'/konto'}>Konto</Link>
                                         </li>
                                         <li>
-                                            <Menu label="Settings">
+                                            <Menu className="p-2" label={
+                                                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" className="bi bi-person-circle" viewBox="0 0 16 16">
+                                                    <path d="M11 6a3 3 0 1 1-6 0 3 3 0 0 1 6 0"/>
+                                                    <path fillRule="evenodd" d="M0 8a8 8 0 1 1 16 0A8 8 0 0 1 0 8m8-7a7 7 0 0 0-5.468 11.37C3.242 11.226 4.805 10 8 10s4.757 1.225 5.468 2.37A7 7 0 0 0 8 1"/>
+                                                </svg>
+                                            }>
                                                 <div className="min-w-[550px]">
                                                     <div
                                                         className="text-mauve12 text-[15px] leading-[19px] font-medium mb-2.5">Dimensions
