3936ae203dc1957226361f524f281d3a472158e2
Joko <kalle0045@gmail.com>
2024-04-03 10:34:46 +0200

    arrow

--- src/Template/TemplateMain.tsx
+++ src/Template/TemplateMain.tsx
@@ -1,15 +1,11 @@
 import {Outlet} from "react-router-dom";
 import Footer from "../component/Footer.tsx";
 import Nav from "../component/Nav.tsx";
-import PopOver from "../component/PopOver.tsx";
 
 const Layout = () => {
     return (
         <div className="flex flex-col min-h-screen">
             <Nav/>
-            <div className="flex">
-                <PopOver />
-            </div>
             <main className="flex-1 flex my-5">
                 <Outlet/>
             </main>
