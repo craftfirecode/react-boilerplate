048df4588331a6584d5fae93303b6be6d8f56e4d
Joko <kalle0045@gmail.com>
2024-04-06 08:44:54 +0200

    remove strapi

--- src/api/ApiService.tsx
+++ src/api/ApiService.tsx
@@ -1,29 +1,34 @@
-import axios from 'axios';
-import qs from 'qs';
+import { useEffect, useState } from "react";
+import axios from "axios";
 
-class ApiService {
-    apiUrl: string;
+const useAxios = (url: string) => {
+    const [data, setData] = useState([]);
+    const [error, setError] = useState<unknown>();
+    const [loading, setLoading] = useState(false);
 
-    constructor() {
-        this.apiUrl = 'https://headless.mapztour.de/';
-    }
+    useEffect(() => {
+        const fetchData = async () => {
+            setLoading(true);
+            try {
+                const response = await axios.get(url);
+                setData(response.data);
+            } catch (error) {
+                setError(error);
+            } finally {
+                setLoading(false);
+            }
+        };
 
-    async fetchGet(path: any, urlParamsObject: any) {
-        try {
-            const queryString = qs.stringify(urlParamsObject);
-            const requestUrl = `${this.apiUrl}/api${path}${queryString ? `?${queryString}` : ""}`;
+        fetchData().then();
 
-            const headers = {
-                Authorization: 'Bearer ' + '1ccae18636a4041f71b524f6c0663372ec0fb092ade356991a2c0d533d482a72f811049572540a81ec52dc9bf0b5e5ab39c10e6d31d059f08f0d773ed5b27712a0c83081043bef33a82e07d5dc9eec21b829383d49080b71abaa96643eaa2de0894d896af7807972c846fba1928fd6ff0e0f623847025229c907c6665b58315f', // Passe dies entsprechend an
-            };
+        return () => {
+            setData([]);
+            setError(null);
+            setLoading(false);
+        };
+    }, [url]);
 
-            const response = await axios.get(requestUrl, { headers });
-            return response.data;
-        } catch (error) {
-            console.error('Fehler beim Abrufen der Daten:', error);
-            throw error; // Wirf den Fehler weiter, damit er von außen behandelt werden kann
-        }
-    }
-}
+    return { data, error, loading };
+};
 
-export default new ApiService();
+export default useAxios;
--- src/pages/Konto.tsx
+++ src/pages/Konto.tsx
@@ -1,10 +1,16 @@
 import {Button} from "../component/Button.tsx";
 import {useTheme} from "../context/ThemeContext.tsx";
 import CtaButton from "../component/CtaButton.tsx";
+import useAxios from "../api/ApiService.tsx";
 
 const Konto = () => {
     const {theme, toggleTheme} = useTheme();
-
+    const { data, error, loading } = useAxios('https://jsonplaceholder.typicode.com/posts');
+    const { data: userData, error: userError, loading: userLoading } = useAxios("https://jsonplaceholder.typicode.com/posts");
+    const { data: postsData, error: postsError, loading: postsLoading } = useAxios("https://jsonplaceholder.typicode.com/posts");
+    console.log(data, error, loading);
+    console.log(userData, userError, userLoading);
+    console.log(postsData, postsError, postsLoading);
     return (
         <div className='container mx-auto'>
             <div className='row'>
