75980ddacd64827a7dd8d939c966e257a9b5ec78
Joko <kalle0045@gmail.com>
2024-04-06 08:29:18 +0200

    remove strapi

--- src/App.tsx
+++ src/App.tsx
@@ -9,38 +9,15 @@ import {ThemeSupa} from "@supabase/auth-ui-shared";
 import {createClient} from '@supabase/supabase-js';
 import Login from "./pages/Login";
 import {useSession} from "./context/useSession";
-import {useEffect, useState} from "react";
-import Page from "./pages/cms/Page.tsx";
-import Post from "./pages/cms/Post.tsx";
-import ApiService from "./api/ApiService.tsx";
 
 const supabase = createClient('https://jfgrqcvupvyzyquawwpg.supabase.co', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImpmZ3JxY3Z1cHZ5enlxdWF3d3BnIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDkyODIwMjEsImV4cCI6MjAyNDg1ODAyMX0.D-O2nSRD3N4WWQOLc-aU3lOWof5tqTx3XriGTEpihDQ')
 
 const App = () => {
-    const { session, isLoading } = useSession(); // Destructure to get session and isLoading
-    const [data, setData] = useState<any>();
-    const urlParamsObject: any = {
-        populate: {
-            nav: {populate: "*"}
-        }
-    };
-    const fetchData = async () => {
-        try {
-            const apiData = await ApiService.fetchGet('/setting', urlParamsObject);
-            setData(apiData.data.attributes.nav);
-        } catch (error) {
-            // Fehlerbehandlung
-        }
-    };
-
-    useEffect(() => {
-        fetchData().then();
-    }, []);
+    const {session, isLoading} = useSession(); // Destructure to get session and isLoading
 
     if (isLoading) {
         return <></>; // Or any other loading indicator
     }
-
 
 
     return (
@@ -50,28 +27,16 @@ const App = () => {
                     <Routes>
                         <Route path="/" element={<TemplateMain/>}>
                             {
-                                !session ? ( // Check if there is no session to decide between Login and Home/Konto routes
+                                !session ? (
                                         <>
-                                            {data.map((item: any, index: any) => (
-                                                <Route key={index} path={item.link}
-                                                       element={<Page pageID={item.page.data.id}/>}/>
-                                            ))}
-                                            <Route path="post/:id" element={<Post/>}/>
-                                            <Route path="/" element={<Login><Auth supabaseClient={supabase} appearance={{theme: ThemeSupa}}/></Login>}/>
+                                            <Route path="/" element={<Login><Auth supabaseClient={supabase}
+                                                                                  appearance={{theme: ThemeSupa}}/></Login>}/>
                                             <Route path="*" element={<NotFound/>}/>
                                         </>
                                     )
                                     :
                                     (
                                         <>
-                                            {data && data.map((item: any, index: any) => (
-                                                <>
-                                                    <Route key={index} path={item.link} element={<Page pageID={item.page.data.id}/>}/>
-                                                    {item.submenu && item.submenu.map((navItem: any, navIndex: any) => (
-                                                        <Route key={navIndex} path={navItem.link} element={<Page pageID={navItem.id}/>}/>
-                                                    ))}
-                                                </>
-                                            ))}
                                             <Route path="/" element={<Home/>}/>
                                             <Route path="/konto" element={<Konto/>}/>
                                             <Route path="*" element={<NotFound/>}/>
--- src/Template/TemplateMain.tsx
+++ src/Template/TemplateMain.tsx
@@ -1,49 +1,11 @@
-import {Link, Outlet} from "react-router-dom";
+import {Outlet} from "react-router-dom";
 import Footer from "../component/Footer.tsx";
 import Nav from "../component/Nav.tsx";
-import {useEffect, useState} from "react";
-import ApiService from "../api/ApiService.tsx";
 
 const Layout = () => {
-    const [data, setData] = useState<any>();
-
-    const urlParamsObject: any = {
-        populate: {
-            nav: {populate: "*"}
-        }
-    };
-    const fetchData = async () => {
-        try {
-            const apiData = await ApiService.fetchGet('/setting', urlParamsObject);
-            setData(apiData.data.attributes.nav);
-        } catch (error) {
-            // Fehlerbehandlung
-        }
-    };
-
-    useEffect(() => {
-        fetchData().then();
-    }, []);
-
     return (
         <div className="flex flex-col min-h-screen">
             <Nav/>
-            {data && data.map((item: any, index: any) => (
-                <div key={index}>
-                    <Link key={item.id}
-                          className="text-dark hover:text-gray-300 px-3 py-2 rounded-md text-sm font-medium"
-                          to={{pathname: item.link}}
-                          state={item}>{item.title}</Link>
-                    {item.submenu && item.submenu.map((subItem: any) => (
-                        <Link key={subItem.id}
-                              className="text-dark hover:text-gray-300 px-3 py-2 rounded-md text-sm font-medium"
-                              to={{pathname: subItem.link}}
-                              state={item}>{subItem.title}</Link>
-                    ))}
-                </div>
-            ))}
-
-
             <main className="flex-1 flex my-5">
                 <Outlet/>
             </main>
