842c1dd6f76dc947924af611db26f0bff9981158
jokoson <deine.email@example.com>
2024-09-13 16:55:29 +0200

    Remove unused code and dependencies

--- src/context/useSession.tsx
+++ src/context/useSession.tsx
@@ -2,7 +2,7 @@ import { useEffect, useState } from "react";
 import { createClient } from '@supabase/supabase-js'
 
 // Initialize the Supabase client
-const supabase = createClient('https://jfgrqcvupvyzyquawwpg.supabase.co', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImpmZ3JxY3Z1cHZ5enlxdWF3d3BnIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDkyODIwMjEsImV4cCI6MjAyNDg1ODAyMX0.D-O2nSRD3N4WWQOLc-aU3lOWof5tqTx3XriGTEpihDQ')
+const supabase = createClient(import.meta.env.VITE_SUPER_URL, import.meta.env.VITE_SUPER_API)
 
 export function useSession() {
     const [session, setSession] = useState<any>(null);
