16f55f99fe2d2cbd2196d1825d53c5a8bd935bd6
jokoson <deine.email@example.com>
2024-09-15 14:06:26 +0200

    Refactor data fetching and subscription logic in Home.tsx

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,35 +1,80 @@
 import { useEffect, useState } from "react";
 import { supabase } from "../supabaseClient";
-import { useSession } from "../context/useSession";
 
 const Home = () => {
   const [data, setData] = useState<any[]>([]);
+
   useEffect(() => {
     const fetchData = async () => {
-      const { data: superData, error } = await supabase.from("maps").select("*");
+      // Initialer Abruf der Daten
+      const { data: initialData, error } = await supabase
+        .from('maps')
+        .select('*');
 
       if (error) {
         console.error("Error fetching data:", error);
-        return [];
+      } else {
+        setData(initialData);
       }
-      setData(superData);
+
+      // Subscription zu den Datenänderungen
+      const subscription = supabase
+        .channel("custom-all-channel")
+        .on(
+          "postgres_changes",
+          { event: "*", schema: "public", table: "maps" },
+          (payload) => {
+            console.log("Change received!", payload);
+            handleDataChange(payload);
+          }
+        )
+        .subscribe();
+
+      // Cleanup bei Unmount
+      return () => {
+        subscription.unsubscribe();
+      };
     };
 
+    // Fetch initial data und set up subscription
     fetchData();
-  }, []); // Add empty dependency array to run the effect only once
+  }, []);
 
+  const handleDataChange = (payload: any) => {
+    const { eventType, new: newData, old: oldData } = payload;
+
+    setData((prevData) => {
+      switch (eventType) {
+        case 'INSERT':
+          // Hinzufügen neuer Einträge
+          return [...prevData, newData];
+
+        case 'UPDATE':
+          // Aktualisieren bestehender Einträge
+          return prevData.map((item) => (item.id === newData.id ? newData : item));
+
+        case 'DELETE':
+          // Entfernen gelöschter Einträge
+          return prevData.filter((item) => item.id !== oldData.id);
+
+        default:
+          return prevData;
+      }
+    });
+  };
 
   return (
     <div>
       <h1>Home</h1>
       <p>This is the Home page</p>
       <h1 className="my-10">Data from Supabase</h1>
-      {data && data.map((map: any) => (
-        <div key={map.id}>
-          <h1>{map.title}</h1>
-          <p>{map.description}</p>
-        </div>
-      ))}
+      {data &&
+        data.map((map: any) => (
+          <div key={map.id}>
+            <h1>{map.title}</h1>
+            <p>{map.description}</p>
+          </div>
+        ))}
     </div>
   );
 };
